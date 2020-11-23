a5f0ddcf6e590fd565528af3e9ca6660e5473cba
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2020-11-23 13:09:13 +0100

    Added ViewModel

---
+++ AuthenTask/TodoList API/ViewModel/ListsCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList_API.ViewModel
+{
+    public class ListsCreate
+    {
+        [Required(ErrorMessage ="Enter Task")]
+        public string Name { get; set; }
+        public string LongNote { get; set; }
+        public DateTimeOffset DueDate { get; set; }
+        public string TasksUpdate { get; set; }
+    }
+}
---
+++ AuthenTask/TodoList API/ViewModel/ListsView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList_API.ViewModel
+{
+    public class ListsView
+    {
+        public string Name { get; set; }
+        public string LongNote { get; set; }
+        public string DueDate { get; set; } = "None";
+        public string TasksUpdate { get; set; } = "Uncompleted";
+    }
+}
