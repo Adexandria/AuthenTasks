0afe6dfbc399fa534846b75a7f4b44230033ff0c
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2021-04-09 16:57:36 +0100

    created a view class for an individual task

---
+++ AuthenTask/TodoList API/ViewModel/ListView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList_API.ViewModel
+{
+    public class ListView
+    {
+        public string Name { get; set; }
+        public string LongNote { get; set; }
+        public string DueDate { get; set; } = null;
+        public string TasksUpdate { get; set; } = "Uncompleted";
+    }
+}
