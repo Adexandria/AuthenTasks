39998df164cd9fae1b3e563733f39de258a85806
Adeola Aderibigbe <adeolaaderibigbe09@gmail.com>
2021-04-09 16:59:02 +0100

    the class now inherits the listview class

--- AuthenTask/TodoList API/ViewModel/ListsView.cs
+++ AuthenTask/TodoList API/ViewModel/ListsView.cs
@@ -1,15 +1,14 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TodoList_API.ViewModel
 {
-    public class ListsView
+    public class ListsView :ListView
     {
-        public string Name { get; set; }
-        public string LongNote { get; set; }
-        public string DueDate { get; set; } = "0-00-0000 00:00:00";
-        public string TasksUpdate { get; set; } = "Uncompleted";
+        public Guid Id { get; set; }
+       
     }
 }
